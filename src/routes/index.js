84e000340cc20f9ef8a6af2e4a815292f90313d0
Simon Altuve <simonc25@gmail.com>
2023-06-23 18:42:55 -0400

    [ADD] documentation added with swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const helmet = require('helmet');
 const compression = require('compression');
 require('express-async-errors');
 const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares')
+const swaggerUI = require('swagger-ui-express');
+const {SWAGGER_PATH} = require('../config');
+const swaggerDocument = require(SWAGGER_PATH);
 
 module.exports = function ({ HomeRoutes,
     UserRoutes,
@@ -29,6 +32,7 @@ module.exports = function ({ HomeRoutes,
     apiRoutes.use('/auth', AuthRoutes);
 
     router.use('/v1/api', apiRoutes);
+    router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
     router.use(NotFoundMiddleware);
     router.use(ErrorMiddleware);
